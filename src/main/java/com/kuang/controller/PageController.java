d5846a3d2b78e30b723fc26b5007fc4f6d1c9aa5
chad <chad@mieasy.com>
2021-02-08 18:39:20 +0800

    thymeleaf

---
+++ src/main/java/com/kuang/controller/PageController.java
@@ -0,0 +1,22 @@
+package com.kuang.controller;
+
+import com.kuang.pojo.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/login")
+    public String index(Model model){
+        model.addAttribute("name","shimei");
+        model.addAttribute("age",24);
+        model.addAttribute("user",new User("@163.com","武汉"));
+        model.addAttribute("html","<h5 style='color: aqua'>测试</h5>");
+        model.addAttribute("list", Arrays.asList(12,13,14));
+        return "login";
+    }
+}
