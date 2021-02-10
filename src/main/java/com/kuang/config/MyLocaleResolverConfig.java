5ce7a65b9526059347abad7f1dcea23cc3810f40
chad <chad@mieasy.com>
2021-02-10 15:21:30 +0800

    测试数据源

--- src/main/java/com/kuang/config/MyLocaleResolverConfig.java
+++ src/main/java/com/kuang/config/MyLocaleResolverConfig.java
@@ -1,5 +1,7 @@
 package com.kuang.config;
 
+import com.alibaba.druid.support.http.StatViewServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
@@ -7,7 +9,10 @@ import org.thymeleaf.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
+
 @Configuration
 public class MyLocaleResolverConfig implements LocaleResolver {
 
@@ -31,5 +36,29 @@ public class MyLocaleResolverConfig implements LocaleResolver {
     @Bean
     public LocaleResolver localeResolver() {
         return new MyLocaleResolverConfig();
+    }
+
+    //配置 Druid 监控管理后台的Servlet；
+//内置 Servlet 容器时没有web.xml文件，所以使用 Spring Boot 的注册 Servlet 方式
+    @Bean
+    public ServletRegistrationBean statViewServlet() {
+        ServletRegistrationBean bean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
+
+        // 这些参数可以在 com.alibaba.druid.support.http.StatViewServlet
+        // 的父类 com.alibaba.druid.support.http.ResourceServlet 中找到
+        Map<String, String> initParams = new HashMap<>();
+        initParams.put("loginUsername", "admin"); //后台管理界面的登录账号
+        initParams.put("loginPassword", "123456"); //后台管理界面的登录密码
+
+        //后台允许谁可以访问
+        //initParams.put("allow", "localhost")：表示只有本机可以访问
+        //initParams.put("allow", "")：为空或者为null时，表示允许所有访问
+        initParams.put("allow", "");
+        //deny：Druid 后台拒绝谁访问
+        //initParams.put("kuangshen", "192.168.1.20");表示禁止此ip访问
+
+        //设置初始化参数
+        bean.setInitParameters(initParams);
+        return bean;
     }
 }
--- src/test/java/com/kuang/SpringbootApplicationTests.java
+++ src/test/java/com/kuang/SpringbootApplicationTests.java
@@ -1,14 +1,37 @@
 package com.kuang;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 class SpringbootApplicationTests {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private DataSource dataSource;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws SQLException {
+//        List<Map<String, Object>> maps = jdbcTemplate.queryForList("SELECT * FROM student_manage.users");
+//        System.out.println(maps);
+//        DataSource dataSource = jdbcTemplate.getDataSource();
+//        System.out.println(dataSource);
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+        DruidDataSource druidDataSource = (DruidDataSource) dataSource;
+        System.out.println("最大连接数："+druidDataSource.getMaxActive());
+        System.out.println("初始化连接数："+druidDataSource.getInitialSize());
     }
 
 }
